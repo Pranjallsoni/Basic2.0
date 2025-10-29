cd97826c51e221e5ab7f46c0ce12bbadf52f6699
Pranjal <shreyakatta2609@gmail.com>
2025-10-29 13:46:01 +0530

    Add files via upload

---
+++ Tutorials.py
@@ -0,0 +1,19 @@
+#TASK 1
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+
+print("Addition: ", add)
+print("Multiplication: ", mul)
+print("Subtraction: ", sub)
+print("Division: ", div)
+
+#TASK2
+a= input("Enter first name: ")
+b= input("Enter last name: ")
+c = a + " " + b
+print("Hello, ",c,"! You are lucky.")
